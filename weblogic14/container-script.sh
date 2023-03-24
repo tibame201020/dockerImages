f16f013915b3f1f335921053deaac5474b0ca65f
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-24 22:36:57 +0800

    Update container-script.sh

--- weblogic14/container-script.sh
+++ weblogic14/container-script.sh
@@ -10,6 +10,11 @@ else
   docker_weblogic14_password=$2
 fi
 
+if [ ! -d "user_projects" ]; then
+  mkdir user_projects
+fi
+
+
 echo "username=$docker_weblogic14_user" > domain.properties
 echo "password=$docker_weblogic14_password" >> domain.properties
 
@@ -18,5 +23,6 @@ docker pull tibame201020/weblogic14
 docker run -d --name wls14 \
              -p 7001:7001 \
              -v $(pwd)/domain.properties:/u01/oracle/properties/domain.properties \
+             -v $(pwd)/user_projects:/u01/oracle/user_projects \
              -e ADMINISTRATION_PORT_ENABLED=false -e DOMAIN_NAME=base_domain \
              tibame201020/weblogic14
