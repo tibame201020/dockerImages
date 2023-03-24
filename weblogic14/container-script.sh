beee43ccf7202f36b3010984dc970897b6473c96
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-24 21:52:21 +0800

    Update container-script.sh

--- weblogic14/container-script.sh
+++ weblogic14/container-script.sh
@@ -1,3 +1,18 @@
+if [ -z "$1" ]; then
+  docker_weblogic14_user="weblogic"
+else
+  docker_weblogic14_user=$1
+fi
+
+if [ -z "$2" ]; then
+  docker_weblogic14_password="weblogic123"
+else
+  docker_weblogic14_password=$1
+fi
+
+export docker_weblogic14_user=docker_weblogic14_user
+export docker_weblogic14_password=docker_weblogic14_password
+
 docker pull tibame201020/weblogic14
 docker run -d --name wls14 \
              -p 7001:7001 \
