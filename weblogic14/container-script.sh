c576c1ab2fa54fe416c277517196ac3b6a7f711d
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-24 21:53:39 +0800

    Update container-script.sh

--- weblogic14/container-script.sh
+++ weblogic14/container-script.sh
@@ -17,5 +17,6 @@ docker pull tibame201020/weblogic14
 docker run -d --name wls14 \
              -p 7001:7001 \
              -v ./:/u01/oracle/properties \
+             -v ./:/u01/oracle/user_projects \
              -e ADMINISTRATION_PORT_ENABLED=false -e DOMAIN_NAME=base_domain \
              tibame201020/weblogic14
