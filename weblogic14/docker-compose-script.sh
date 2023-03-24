ce8bede65c4c2b975e1dd816a7964824dccfbd9a
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-24 21:52:39 +0800

    Update docker-compose-script.sh

--- weblogic14/docker-compose-script.sh
+++ weblogic14/docker-compose-script.sh
@@ -1,2 +1,17 @@
+if [ -z "$1" ]; then
+  docker_weblogic14_user="weblogic"
+else
+  docker_weblogic14_user=$1
+fi
+
+if [ -z "$2" ]; then
+  docker_weblogic14_password="weblogic123"
+else
+  docker_weblogic14_password=$1
+fi
+
+export docker_weblogic14_user=docker_weblogic14_user
+export docker_weblogic14_password=docker_weblogic14_password
+
 docker pull tibame201020/weblogic14
 docker compose up
