f8bd61b5da6ab4b8eed7d34fa1b4935b7697cbfb
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-25 15:43:17 +0800

    Update start-angualr-dev.sh

--- angular-cli-env/start-angualr-dev.sh
+++ angular-cli-env/start-angualr-dev.sh
@@ -1,1 +1,2 @@
-docker run -it --rm --name=angular-dev -p 4200:4200 -v $1:/app -w /app angular-develop bash -c "npm install && ng serve --host 0.0.0.0"
+docker pull tibame201020/angular-develop
+docker run -it --rm --name=angular-dev -p 4200:4200 -v $1:/app -w /app tibame201020/angular-develop bash -c "npm install && ng serve --host 0.0.0.0"
