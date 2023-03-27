431172d7f9d39c913c551ccdc67191717c7ffb02
TSRI\Jeff <jeff.hsu@frog-jump.com>
2023-03-27 08:27:31 +0800

    angular project generator

---
+++ angular-cli-env/angular_project_generator/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -d "codes" ]; then
+  mkdir codes
+fi
+
+docker build -t angular_project_generator . -f dockerfiler.angular_project_generator
+docker run -it --rm --name=angular_project_generator -v $(pwd)/codes:/app angular_project_generator
