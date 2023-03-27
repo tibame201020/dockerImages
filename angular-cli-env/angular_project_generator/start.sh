f3efcc83a28b6962d313090377e8fc4e6d52d1b3
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-03-27 08:50:42 +0800

    Update start.sh

--- angular-cli-env/angular_project_generator/start.sh
+++ angular-cli-env/angular_project_generator/start.sh
@@ -2,5 +2,5 @@
 if [ ! -d "codes" ]; then
   mkdir codes
 fi
-docker pull tibame201020/angular_project_generator
+docker pull tibame201020/angular_project_generator:latest
 docker run -it --rm --name=angular_project_generator -v $(pwd)/codes:/app angular_project_generator
