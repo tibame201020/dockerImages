397596196310aa2048dd968090fe3e0f0dcc4618
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-06-07 13:34:05 +0800

    Update container.sh

--- elasticsearch/container.sh
+++ elasticsearch/container.sh
@@ -1,1 +1,11 @@
-docker run -d --rm -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -e "xpack.security.enabled=false" --name=elasticsearch  docker.elastic.co/elasticsearch/elasticsearch:8.7.1
+if [ ! -d "esdata" ]; then
+  mkdir esdata
+fi
+
+docker run -d --rm\
+ -v $(pwd)/esdata:/usr/share/elasticsearch/data\
+ -p 9200:9200 -p 9300:9300\
+ -e "discovery.type=single-node"\
+ -e "xpack.security.enabled=false"\
+ --name=elasticsearch\
+ docker.elastic.co/elasticsearch/elasticsearch:8.7.1
