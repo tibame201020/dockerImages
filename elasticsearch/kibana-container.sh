da619ddc08db329a789cde60baeaed43df401e1d
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-06-07 11:25:59 +0800

    Update kibana-container.sh

--- elasticsearch/kibana-container.sh
+++ elasticsearch/kibana-container.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name=kibana --link elasticsearch:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:8.8.0
+docker run -d --rm --name=kibana --link elasticsearch:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:8.7.1
