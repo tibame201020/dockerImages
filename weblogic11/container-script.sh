c6459f527db1af786d51c600d93d24f3803d3df6
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-06-20 11:33:37 +0800

    Create container-script.sh

---
+++ weblogic11/container-script.sh
@@ -0,0 +1,31 @@
+if [ -z "$1" ]; then
+  docker_weblogic11_user="weblogic"
+else
+  docker_weblogic11_user=$1
+fi
+
+if [ -z "$2" ]; then
+  docker_weblogic11_password="weblogic123"
+else
+  docker_weblogic11_password=$2
+fi
+
+if [ ! -d "user_projects" ]; then
+  mkdir user_projects
+fi
+
+if [ ! -d "home" ]; then
+  mkdir home
+fi
+
+echo "username=$docker_weblogic11_user" > boot.properties
+echo "password=$docker_weblogic11_password" >> boot.properties
+
+
+docker run -d --name wls11 --rm \
+             -p 6008:7001 \             
+             -v $(pwd)/boot.properties:/u01/app/oracle/middleware/user_projects/domains/base_domain/servers/AdminServer/security/boot.properties \
+             -v $(pwd)/user_projects:/u01/app/oracle/middleware/user_projects \
+             -v $(pwd)/home:/home \
+             -e ADMINISTRATION_PORT_ENABLED=false -e DOMAIN_NAME=base_domain -e TZ=Asia/Taipei \
+             tibame201020/weblogic11
