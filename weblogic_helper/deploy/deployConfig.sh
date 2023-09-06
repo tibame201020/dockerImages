470134a59ef0a3a255b6bfbeeb704e3acf8c0d9b
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-09-06 11:06:43 +0800

    Create deployConfig.sh

---
+++ weblogic_helper/deploy/deployConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wlsVersion=10 (weblogic version 10 or 14)
+warFile=xxx (application name)
+warPath=mnt/d/Dev/program/wars (war path)
+targetServer=AdminServer
+hostIp=172.17.0.2:6008(if weblogic run with container use docker ip)
+wlsAcc=weblogic
+wlsPwd=weblogic123
+weblogichelper=weblogichelper(weblogic_herlper container name)
