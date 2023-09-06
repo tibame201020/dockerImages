1e14543927b5abd118b586f13b130dd6a6719c8c
tibame201020 <78014702+tibame201020@users.noreply.github.com>
2023-09-06 11:11:46 +0800

    Create addJndiConfig.sh

---
+++ weblogic_helper/jndi/addJndiConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wlsVersion=14 (weblogic version 10 or 14)
+jsonFile=dsProps (json file name)
+jsonFilePath=/mnt/c/Users/jdbc (json file path)
+hostIp=127.0.0.1 (if weblogic run with docker container use docker ip)
+wlsAcc=weblogic
+wlsPwd=weblogic123
+weblogichelper=weblogichelper (weblogic_helper container name)
